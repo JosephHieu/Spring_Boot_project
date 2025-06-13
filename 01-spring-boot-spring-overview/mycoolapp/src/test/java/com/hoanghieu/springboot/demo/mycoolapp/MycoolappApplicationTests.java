e69c85a7f2a7ce912421604a017f7c3d431f7689
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-06-13 21:24:06 +0700

    second commit

---
+++ 01-spring-boot-spring-overview/mycoolapp/src/test/java/com/hoanghieu/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoanghieu.springboot.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
