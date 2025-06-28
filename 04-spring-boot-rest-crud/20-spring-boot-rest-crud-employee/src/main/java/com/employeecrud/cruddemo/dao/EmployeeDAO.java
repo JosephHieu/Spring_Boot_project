30cfa377674a2bf14455d3480dffefacfcb09228
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-06-28 15:39:28 +0700

    spring security

---
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/employeecrud/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.employeecrud.cruddemo.dao;
+
+import com.employeecrud.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
---
+++ 04-spring-boot-rest-crud/30-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/employeecrud/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.employeecrud.cruddemo.service;
+
+import com.employeecrud.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
