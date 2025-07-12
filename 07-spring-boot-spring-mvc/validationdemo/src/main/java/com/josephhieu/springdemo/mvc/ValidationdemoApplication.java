47ba7f06ef643370a076aaa8415f28249035d699
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-07-12 21:00:16 +0700

    Custom Validation

--- 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/josephhieu/springdemo/mvc/ValidationdemoApplication.java
+++ 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/josephhieu/springdemo/mvc/ValidationdemoApplication.java
@@ -1,4 +1,4 @@
-package com.josephhieu.springdemo.mvc.validationdemo;
+package com.josephhieu.springdemo.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ValidationdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ValidationdemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationdemoApplication.class, args);
+    }
 
 }
