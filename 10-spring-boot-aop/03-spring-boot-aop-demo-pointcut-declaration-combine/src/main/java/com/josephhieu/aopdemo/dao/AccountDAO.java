cffefc38fb1692594390be2f9939efb5b7b80b46
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-08-27 16:28:26 +0700

    @AfterReturning Advice

--- 10-spring-boot-aop/03-spring-boot-aop-demo-pointcut-declaration-combine/src/main/java/com/josephhieu/aopdemo/dao/AccountDAO.java
+++ 10-spring-boot-aop/03-spring-boot-aop-demo-pointcut-declaration-combine/src/main/java/com/josephhieu/aopdemo/dao/AccountDAO.java
@@ -2,18 +2,22 @@ package com.josephhieu.aopdemo.dao;
 
 import com.josephhieu.aopdemo.Account;
 
+import java.util.List;
+
 public interface AccountDAO {
 
     void addAccount(Account theAccount, boolean vipFlag);
 
     boolean doWork();
 
-    public String getName();
+    String getName();
 
-    public void setName(String name);
+    void setName(String name);
 
-    public String getServiceCode();
+    String getServiceCode();
 
-    public void setServiceCode(String serviceCode);
+    void setServiceCode(String serviceCode);
 
+    // add a new method: findAccounts()
+    List<Account> findAccounts();
 }
--- 10-spring-boot-aop/03-spring-boot-aop-demo-pointcut-declaration-combine/src/main/java/com/josephhieu/aopdemo/dao/AccountDAOImpl.java
+++ 10-spring-boot-aop/03-spring-boot-aop-demo-pointcut-declaration-combine/src/main/java/com/josephhieu/aopdemo/dao/AccountDAOImpl.java
@@ -3,6 +3,9 @@ package com.josephhieu.aopdemo.dao;
 import com.josephhieu.aopdemo.Account;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class AccountDAOImpl implements AccountDAO {
 
@@ -40,5 +43,23 @@ public class AccountDAOImpl implements AccountDAO {
     public void setServiceCode(String serviceCode) {
         System.out.println(getClass() + ": in setServiceCode()");
         this.serviceCode = serviceCode;
+    }
+
+    @Override
+    public List<Account> findAccounts() {
+
+        List<Account> myAccounts = new ArrayList<>();
+
+        // create sample accounts
+        Account temp1 = new Account("John", "Silver");
+        Account temp2 = new Account("Ronaldo", "Goat");
+        Account temp3 = new Account("Messi", "second rate of all time");
+
+        // add them to our accounts list
+        myAccounts.add(temp1);
+        myAccounts.add(temp2);
+        myAccounts.add(temp3);
+
+        return myAccounts;
     }
 }
